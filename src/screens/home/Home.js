0aa02ffd75e180cd7053c551000bfdfaa9b9030e
Aditya Peesapati <aditya.psrv@gmail.com>
2021-05-28 19:11:24 +0530

    Adding Upcoming movies section to home screen

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from '../../common/header/Header';
+import UpcomingMovies from "./homecomponents/UpcomingMovies";
+import './Home.css';
 
 const Home = function () {
     return (
-        <Header/>
+        <div>
+            <Header/>
+            <div className = 'upcoming-movies-heading'>Upcoming Movies</div>
+            <UpcomingMovies/>
+        </div>
     )
 }
 
